ba5caf9a744c93b89e7c209bd2d89adbe99f8db0
SatamSai <75394053+SatamSai@users.noreply.github.com>
2022-06-14 10:30:44 +0530

    Update urls.py

--- server/urls.py
+++ server/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from app import views
 from django.conf import settings
 from django.conf.urls.static import static
-import pandas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
