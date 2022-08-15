d1f35537347eb80c55a201282ce33d8d34cf4f72
SatamSai <75394053+SatamSai@users.noreply.github.com>
2022-08-15 16:58:44 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     'https://aerotest.netlify.app',
 )
